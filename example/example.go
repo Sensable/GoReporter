768467b976a8492935ab0fc905342c1ad131bbd2
Massimiliano Marcon <me@marcon.me>
2014-07-25 07:59:24 +0200

    Applies Gofmt formatting

--- example/example.go
+++ example/example.go
@@ -5,43 +5,43 @@ for Sensable.
 package main
 
 import (
-        "github.com/sensable/GoReporter";
-        "time";
-        "fmt"
-    )
+	"fmt"
+	"github.com/sensable/GoReporter"
+	"time"
+)
 
 const requestbinUrl = "http://requestb.in/1696gmd1"
 
 func main() {
-    sensable := GoReporter.Sensable {
-        SensorId: "some-id",
-        Unit: "°c",
-        SensorType: "temperature",
-        Location: GoReporter.Location {
-            Latitude: 52.5,
-            Longitude: 13.3,
-        },
-        Name: "some name here",
-    }
+	sensable := GoReporter.Sensable{
+		SensorId:   "some-id",
+		Unit:       "°c",
+		SensorType: "temperature",
+		Location: GoReporter.Location{
+			Latitude:  52.5,
+			Longitude: 13.3,
+		},
+		Name: "some name here",
+	}
 
-    settings := GoReporter.Settings {
-        AccessToken: "some-access-token",
-        Private: false,
-    }
+	settings := GoReporter.Settings{
+		AccessToken: "some-access-token",
+		Private:     false,
+	}
 
-    fmt.Println("Check result at " + requestbinUrl + "?inspect")
+	fmt.Println("Check result at " + requestbinUrl + "?inspect")
 
-    reporter := sensable.BuildReporter(settings, requestbinUrl)
+	reporter := sensable.BuildReporter(settings, requestbinUrl)
 
-    sample := GoReporter.Sample {
-        Value: 32.5,
-        Timestamp: int64(time.Now().UnixNano() / 1e6),
-        State: "it's getting warmer",
-    }
+	sample := GoReporter.Sample{
+		Value:     32.5,
+		Timestamp: int64(time.Now().UnixNano() / 1e6),
+		State:     "it's getting warmer",
+	}
 
-    _, err := reporter.Report(sample)
+	_, err := reporter.Report(sample)
 
-    if err != nil {
-        fmt.Println("Something went wrong", err)
-    }
+	if err != nil {
+		fmt.Println("Something went wrong", err)
+	}
 }
--- reporter.go
+++ reporter.go
@@ -5,11 +5,11 @@ and methods to post sensed data to http://sensable.io.
 package GoReporter
 
 import (
-        "encoding/json";
-        "net/http";
-        "bytes";
-        "errors"
-        )
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+)
 
 /*
 Default URI where to reach the Sensable API.
@@ -20,71 +20,71 @@ const SensableApiUri = "http://sensable.io/sensable"
 Sensable is an object that describe a sensor.
 */
 type Sensable struct {
-    SensorId string `json:"sensorid"`
-    Unit string `json:"unit"`
-    SensorType string `json:"sensortype"`
-    Location Location `json:"location"`
-    Name string `json:"name"`
+	SensorId   string   `json:"sensorid"`
+	Unit       string   `json:"unit"`
+	SensorType string   `json:"sensortype"`
+	Location   Location `json:"location"`
+	Name       string   `json:"name"`
 }
 
-func (sensable Sensable) BuildReporter(settings Settings, uri... string) (reporter Reporter) {
-    sensableUri := SensableApiUri
-    if uri != nil {
-        sensableUri = uri[0]
-    }
-    reporter = Reporter {
-        uri: sensableUri,
-        settings: settings,
-        sensable: sensable,
-    }
-    return
+func (sensable Sensable) BuildReporter(settings Settings, uri ...string) (reporter Reporter) {
+	sensableUri := SensableApiUri
+	if uri != nil {
+		sensableUri = uri[0]
+	}
+	reporter = Reporter{
+		uri:      sensableUri,
+		settings: settings,
+		sensable: sensable,
+	}
+	return
 }
 
 /*
 Settings to be applied to this reporter.
 */
 type Settings struct {
-    AccessToken string `json:"accessToken"` //the accessToken that can be found in your Sensable account
-    Private bool `json:"-"` //indicates whether or not this Sensable should be kept private (currently ignored)
+	AccessToken string `json:"accessToken"` //the accessToken that can be found in your Sensable account
+	Private     bool   `json:"-"`           //indicates whether or not this Sensable should be kept private (currently ignored)
 }
 
 /*
 Sample is a sample captured by the Sensable.
 */
 type Sample struct {
-    Value float64 `json:"value"`
-    Timestamp int64 `json:"timestamp"`
-    State string `json:"state"`
+	Value     float64 `json:"value"`
+	Timestamp int64   `json:"timestamp"`
+	State     string  `json:"state"`
 }
 
 /*
 Location is the geolocation associated with this Sensable.
 */
 type Location struct {
-    Latitude float64 `json:"latitude"`
-    Longitude float64 `json:"longitude"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
 }
 
 type payload struct {
-    Sensable
-    Settings
-    Sample Sample `json:"sample"`
+	Sensable
+	Settings
+	Sample Sample `json:"sample"`
 }
 
 func (payload payload) upload(uri string) (bool, error) {
-    b, err := json.Marshal(payload)
-    if err != nil {
-        return false, err
-    }
-    res, err := http.Post(uri, "application/json", bytes.NewReader(b));
-    defer res.Body.Close();
-    if err != nil {
-        return false, err
-    }
-    if res.StatusCode == 200 {
-        return true, nil
-    }
-    return false, errors.New("Server responded with " + res.Status)
+	b, err := json.Marshal(payload)
+	if err != nil {
+		return false, err
+	}
+	res, err := http.Post(uri, "application/json", bytes.NewReader(b))
+	defer res.Body.Close()
+	if err != nil {
+		return false, err
+	}
+	if res.StatusCode == 200 {
+		return true, nil
+	}
+	return false, errors.New("Server responded with " + res.Status)
 }
 
 /*
@@ -92,25 +92,25 @@ Reporter is the object that takes care of reporting new
 samples for the given Sensable via the Sensable API.
 */
 type Reporter struct {
-    uri string
-    settings Settings
-    sensable Sensable
+	uri      string
+	settings Settings
+	sensable Sensable
 }
 
 func (reporter Reporter) Report(sample Sample) (bool, error) {
-    payload := payload {
-        Sensable: Sensable {
-            reporter.sensable.SensorId,
-            reporter.sensable.Unit,
-            reporter.sensable.SensorType,
-            reporter.sensable.Location,
-            reporter.sensable.Name,
-        },
-        Settings: Settings {
-            reporter.settings.AccessToken,
-            reporter.settings.Private,
-        },
-        Sample: sample,
-    }
-    return payload.upload(reporter.uri)
+	payload := payload{
+		Sensable: Sensable{
+			reporter.sensable.SensorId,
+			reporter.sensable.Unit,
+			reporter.sensable.SensorType,
+			reporter.sensable.Location,
+			reporter.sensable.Name,
+		},
+		Settings: Settings{
+			reporter.settings.AccessToken,
+			reporter.settings.Private,
+		},
+		Sample: sample,
+	}
+	return payload.upload(reporter.uri)
 }
